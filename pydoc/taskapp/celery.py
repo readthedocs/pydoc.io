2c6c7e3272dc85dfd8b8b0a35bcee31efc67e68a
Eric Holscher <eric@ericholscher.com>
2016-10-20 14:55:49 -0700

    Use straight sphinx build

--- pydoc/taskapp/celery.py
+++ pydoc/taskapp/celery.py
@@ -85,7 +85,7 @@ def _build_docs(project, version, project_url, project_filename):
         if not os.path.exists(outdir.root):
             os.makedirs(outdir.root)
         print('Running Sphinx')
-        sphinx_command = '/Users/eric/.virtualenvs/pydoc/bin/sphinx-build -b html -d {root}_build/{name}-doctrees {root} {outdir}'.format(
+        sphinx_command = 'sphinx-build -b html -d {root}_build/{name}-doctrees {root} {outdir}'.format(
             outdir=outdir.root,
             root=extract_dir,
             name=directory_name
