15873f3f196908c274e4802ea422613952235116
Eric Holscher <eric@ericholscher.com>
2016-11-04 11:22:32 -0700

    Add ability to build from builds page

--- pydoc/core/views.py
+++ pydoc/core/views.py
@@ -1,10 +1,16 @@
-import os
-
 from django.views.generic import TemplateView
-from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+
+from django import forms
 
 from pydoc.taskapp.celery import build
 from pydoc.core.models import Release
+
+
+class BuildForm(forms.Form):
+    project = forms.CharField(label='Project', max_length=100)
 
 
 class HomeView(TemplateView):
@@ -15,10 +21,24 @@ class HomeView(TemplateView):
         return Release.objects.filter(built=True)
 
 
-class BuildView(TemplateView):
+class BuildView(View):
+    form_class = BuildForm
+    # initial = {'project': 'requests'}
     template_name = "pages/build.html"
     title = "Pydoc Build"
 
-    def build(self):
-        project = self.request.GET.get('project', 'requests')
-        build.delay(project=project)
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        success = False
+        if form.is_valid():
+            # <process form cleaned data>
+            project = form.cleaned_data['project']
+            build.delay(project=project)
+            success = True
+        return render(request, self.template_name, {
+            'form': form, 'success': success
+        })
