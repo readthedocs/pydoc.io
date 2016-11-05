f0e3a0f72e31d1bf86f4463032a082435c3e9651
Eric Holscher <eric@ericholscher.com>
2016-11-04 17:14:44 -0700

    Clean up other built setting logic.

--- pydoc/core/management/commands/process_freeze.py
+++ pydoc/core/management/commands/process_freeze.py
@@ -6,7 +6,6 @@ pypi.
 import sys
 from django.core.management.base import BaseCommand
 from pydoc.taskapp.celery import build
-from pydoc.core.pypi import create_or_update_release
 
 
 class Command(BaseCommand):
@@ -17,7 +16,4 @@ class Command(BaseCommand):
             line = line.strip()
             package, version = line.split('==')
             print("Building %s:%s" % (package, version))
-            release = create_or_update_release(package=package, release=version)
-            release.built = True
-            release.save()
             build.delay(project=package, version=version)
