396a2c4789b61b64639da7d6388c1ea8493d6f57
Eric Holscher <eric@ericholscher.com>
2017-04-06 11:54:32 -0700

    Fix tests

---
+++ pydoc/core/test_views.py
@@ -0,0 +1,18 @@
+import unittest
+from django.test import Client
+import pytest
+
+pytestmark = pytest.mark.django_db
+
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
