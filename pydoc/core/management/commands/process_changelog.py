ac6c979667c5952d664d43666aedb1f168fb8f3b
Eric Holscher <eric@ericholscher.com>
2017-04-07 15:45:36 -0700

    Configure minutes in process_changelog

--- pydoc/core/management/commands/process_changelog.py
+++ pydoc/core/management/commands/process_changelog.py
@@ -21,10 +21,18 @@ class Command(BaseCommand):
             help='Build changelog as well as update',
         )
 
+        parser.add_argument(
+            '--minutes',
+            dest='minutes',
+            type=int,
+            default=120,
+            help='Number of minutes to go back',
+        )
+
     def handle(self, *args, **options):
         if options['build']:
             print('Building changelog')
-            build_changelog(hours=2)
+            build_changelog(minutes=options['minutes'])
         else:
             since = datetime.datetime.utcnow() - datetime.timedelta(days=1)
             print("updating change from pypi since %s" % since)
