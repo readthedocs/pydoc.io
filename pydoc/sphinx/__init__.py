4d0ac26883bbd72c107ad8a91aeb159ea7d4c480
Eric Holscher <eric@ericholscher.com>
2017-11-06 15:11:24 -0700

    Really enable analytics this time

--- pydoc/sphinx/__init__.py
+++ pydoc/sphinx/__init__.py
@@ -4,6 +4,10 @@ import os
 import environ
 import json
 import copy
+import logging
+
+log = logging.getLogger(__name__)
+
 
 #  Whitelist keys that we want to output
 KEYS = [
@@ -41,16 +45,42 @@ def update_body(app, pagename, templatename, context, doctree):
             os.makedirs(out_dir())
         out_file = json_dir.path(pagename + '.json')
         to_write = open(out_file(), 'w+')
-        to_context = copy.deepcopy(context)
+        to_context = copy.copy(context)
         # Use list here so we don't get an error on changing dict during iteration
         for key in list(context):
             if key not in KEYS:
                 del to_context[key]
         to_write.write(json.dumps(to_context, indent=4))
-    except Exception as e:
-        print('ERRRRRRRRR: ', e)
-        raise
+    except Exception:
+        log.exception('Failure in JSON search dump')
+
+
+def add_ga_javascript(app, pagename, templatename, context, doctree):
+    """
+    From the sphinxcontrib.googleanalytics package
+    """
+    if not app.config.googleanalytics_enabled:
+        return
+
+    metatags = context.get('metatags', '')
+    metatags += """<script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '%s']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + \
+            '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>""" % app.config.googleanalytics_id
+    context['metatags'] = metatags
 
 
 def setup(app):
     app.connect('html-page-context', update_body)
+    app.add_config_value('googleanalytics_id', '', 'html')
+    app.add_config_value('googleanalytics_enabled', True, 'html')
+    app.connect('html-page-context', add_ga_javascript)
